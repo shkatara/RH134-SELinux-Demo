6a5b79827c51f72a3657e567a133029214887e4a
root <root@shubham.lab.example.com>
2018-04-11 11:56:38 +0530

    added location to /home

--- mkdir.py
+++ mkdir.py
@@ -4,5 +4,5 @@ import cgi
 print "Content-type:text/html\r\n\r\n"
 form = cgi.FieldStorage() 
 first_name = form.getvalue('firstname')
-a=getstatusoutput('mkdir /tmp/{0}'.format(first_name))
+a=getstatusoutput('sudo /home/{0}'.format(first_name))
 print a
